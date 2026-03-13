d32cf6562cd72744c45eb63a1e8f11fba5185ee7
CLEVERSON  SANTOS <felipecleverson1@gmail.com>
2026-03-13 12:42:33 -0400

    operadoresAritimeticos

---
+++ src/Fundamentos_teoricos/operadoresAritimeticos.java
@@ -0,0 +1,17 @@
+package Fundamentos_teoricos;
+
+public class operadoresAritimeticos {
+
+    private static int c;
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int c = a / b;
+
+        System.out.println(c);
+
+
+    }
+}
