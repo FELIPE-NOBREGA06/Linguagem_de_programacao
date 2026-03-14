b051a822bcc14de5e18abe3ad082df2f4b389f29
Felipe Cleverson <felipecleverson1@gmail.com>
2026-03-14 18:55:05 -0400

    OperadooresLogicos

---
+++ src/Fundamentos_teoricos/OperadoresLogicos.java
@@ -0,0 +1,15 @@
+package Fundamentos_teoricos;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 10;
+        int c = 3;
+        int d = 4;
+
+        boolean resultado = a == b && c == d;
+        System.out.println(resultado);
+
+    }
+}
