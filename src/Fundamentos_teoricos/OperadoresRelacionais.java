8cf4c62fe07c4861731d1a84874713d7200a7edc
Felipe Cleverson <felipecleverson1@gmail.com>
2026-03-14 14:49:05 -0400

    OperadoresRelacionais

---
+++ src/Fundamentos_teoricos/OperadoresRelacionais.java
@@ -0,0 +1,16 @@
+package Fundamentos_teoricos;
+
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 10;
+
+        boolean resultado = a  != b;
+
+        System.out.println(resultado);
+
+
+
+    }
+}
