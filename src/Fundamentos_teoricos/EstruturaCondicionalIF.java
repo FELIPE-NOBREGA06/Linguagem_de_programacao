0a4259b858702435eca35bcc6280e7bf43949c97
Felipe Cleverson <felipecleverson1@gmail.com>
2026-03-15 09:31:08 -0400

    EstrturaCondicionalIf

---
+++ src/Fundamentos_teoricos/EstruturaCondicionalIF.java
@@ -0,0 +1,15 @@
+package Fundamentos_teoricos;
+
+public class EstruturaCondicionalIF {
+
+    public static void main(String[] args) {
+        int a  = 15;
+        int b = 10;
+
+        if(a < b){
+            System.out.println("a é menor que b");
+        }else{
+            System.out.println("b é menor que a ");
+        }
+    }
+}
