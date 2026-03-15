294be6c28107f95fadf467b98de2d9a72eff400c
Felipe Cleverson <felipecleverson1@gmail.com>
2026-03-15 09:38:08 -0400

    OperadoresTernario

---
+++ src/Fundamentos_teoricos/OperadorTernario.java
@@ -0,0 +1,12 @@
+package Fundamentos_teoricos;
+
+public class OperadorTernario {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        boolean resultado = a > b ? true : false;
+
+        System.out.println(resultado);
+    }
+}
