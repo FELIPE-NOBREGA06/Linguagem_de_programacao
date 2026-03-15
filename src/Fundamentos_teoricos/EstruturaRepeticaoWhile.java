b5d555d8274a6477878769acef2844baf78ca79d
Felipe Cleverson <felipecleverson1@gmail.com>
2026-03-15 14:35:00 -0400

    EstruturaRepeticaoWhile

---
+++ src/Fundamentos_teoricos/EstruturaRepeticaoWhile.java
@@ -0,0 +1,12 @@
+package Fundamentos_teoricos;
+
+public class EstruturaRepeticaoWhile {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        while (numero <= 4){
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
